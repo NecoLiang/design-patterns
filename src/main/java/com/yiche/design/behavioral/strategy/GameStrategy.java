3cef722ba72132514ca06eeff1de954594222fb4
liangyutao <liangyutao@yiche.com>
2021-03-18 10:25:04 +0800

    模板模式、策略模式、状态模式、观察者模式、中介模式

---
+++ src/main/java/com/yiche/design/behavioral/strategy/GameStrategy.java
@@ -0,0 +1,10 @@
+package com.yiche.design.behavioral.strategy;
+
+/**
+ * 游戏策略
+ */
+public interface GameStrategy {
+
+    //战斗策略
+    void warStrategy();
+}
---
+++ src/main/java/com/yiche/design/behavioral/template/MainTest.java
@@ -0,0 +1,12 @@
+package com.yiche.design.behavioral.template;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+
+        AutoCookMachine cookMachine = new AutoCookMachine();
+
+        cookMachine.cook();
+
+    }
+}
